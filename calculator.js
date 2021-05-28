bbfa3322a5a3897ffc0eff2621253eff7d63b770
cjorqueravocos <cjorqueravocos@gmail.com>
2021-05-28 14:20:06 -0300

    onclick addition

---
+++ calculator.js
@@ -0,0 +1,7 @@
+function addition (){
+    var number1 = parseFloat(document.getElementById('number1').value);
+    var number2 = parseFloat(document.getElementById('number2').value);
+    var resultado = number1 + number2;
+
+    document.getElementById('result').value = result;
+}
